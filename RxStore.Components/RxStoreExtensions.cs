8ceef9d718984f546b9ed8144a04b7a62fca5340
George Darchiashvili <gdar91@live.com>
2020-09-14 00:57:08 +0400

    fixed signalr observable missing oncompleted.

--- RxStore.Components/RxStoreExtensions.cs
+++ RxStore.Components/RxStoreExtensions.cs
@@ -62,6 +62,10 @@ namespace RxStore
                             {
                                 observer.OnError(e);
                             }
+                            else
+                            {
+                                observer.OnCompleted();
+                            }
 
                             return Task.CompletedTask;
                         };
