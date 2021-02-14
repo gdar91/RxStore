c6a6a80f6b75540c5c937af9913a85d3ac195db9
George Darchiashvili <gdar91@live.com>
2021-02-15 02:42:25 +0400

    added locations and pure router.

---
+++ RxStore.ForRazor/Locations.cs
@@ -0,0 +1,33 @@
+﻿using Fills;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+using System;
+using System.Reactive.Linq;
+
+namespace RxStore
+{
+    public sealed class Locations : IObservable<string>
+    {
+        private readonly IObservable<string> observable;
+
+
+        public Locations(NavigationManager navigationManager)
+        {
+            observable =
+                Observable
+                    .FromEventPattern<LocationChangedEventArgs>(
+                        handler => navigationManager.LocationChanged += handler,
+                        handler => navigationManager.LocationChanged -= handler
+                    )
+                    .Select(eventPatter => eventPatter.EventArgs.Location)
+                    .StartWith(() => navigationManager.Uri)
+                    .Select(navigationManager.ToBaseRelativePath)
+                    .DistinctUntilChanged()
+                    .ReplayScoped(1)
+                    .RefCount();
+        }
+
+
+        public IDisposable Subscribe(IObserver<string> observer) => observable.Subscribe(observer);
+    }
+}
